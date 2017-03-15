da4dec56d66ade57db0c5d17e1285535c8baec1b
bigmaoniu <joe.zhou@reactivecore.com>
2017-03-15 16:55:24 -0400

    fix(validation.module): add exports

--- src/validation.module.ts
+++ src/validation.module.ts
@@ -4,11 +4,9 @@ import { EqualValidator } from './directives/equal-validator.directive';
 import { SqlValidator } from './directives/sql-validator.directive';
 import { FileUploaderDirective } from './directives/file-uploader.directive';
 import { BrowserModule } from '@angular/platform-browser';
-import { BrowserAnimationsModule } from '@angular/platform-browser/typings/animations';
 @NgModule({
   imports: [
     BrowserModule,
-    BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
   ],
@@ -16,9 +14,14 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/typings/anima
     SqlValidator,
     FileUploaderDirective,
     EmailValidator,
-    EqualValidator,
+    EqualValidator
   ],
-  exports: []
+  exports: [
+    SqlValidator,
+    FileUploaderDirective,
+    EmailValidator,
+    EqualValidator
+  ]
 })
 export class NgxValidation {
 
