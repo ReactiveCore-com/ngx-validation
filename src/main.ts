749d4dfc3e4a3a6da8ba04c562fffc1db3c0e04b
chris portscheller <christopher.portscheller@reactivecore.com>
2017-11-08 08:44:40 -0500

    fix(upgrade): upgrade to Angular5

--- src/main.ts
+++
@@ -1,4 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { NgValidationModule } from './app';
-
-platformBrowserDynamic().bootstrapModule(NgValidationModule);
