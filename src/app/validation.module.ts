00063cdc86a337377ea6422f8db20e23503b5fd6
chris portscheller <christopher.portscheller@reactivecore.com>
2017-11-09 11:25:06 -0500

    fix(build): Remove sql-parser

--- src/app/validation.module.ts
+++ src/app/validation.module.ts
@@ -2,19 +2,16 @@ import { NgModule } from '@angular/core';
 
 import { EmailValidator } from './directives/email-validator.directive';
 import { EqualValidator } from './directives/equal-validator.directive';
-import { SqlValidator } from './directives/sql-validator.directive';
 import { FileTypeValidator } from './directives/file-type-validator.directive';
 
 @NgModule({
   imports: [],
   declarations: [
-    SqlValidator,
     FileTypeValidator,
     EmailValidator,
     EqualValidator
   ],
   exports: [
-    SqlValidator,
     FileTypeValidator,
     EmailValidator,
     EqualValidator
